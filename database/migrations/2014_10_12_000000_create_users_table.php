8a181b08adba2b5718d4b6d425348e2256b9a92b
philliprearick <psrearick@gmail.com>
2021-05-11 04:51:20 -0400

    add author bio and image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image_url');
+            $table->text('bio');
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/js/Pages/Authors/Index.vue
+++ resources/js/Pages/Authors/Index.vue
@@ -5,40 +5,12 @@
             <h1 class="text-2xl font-bold text-center">{{ category.name }}</h1>
             <inertia-link class="text-yellow-500 text-sm" href="/posts/posts">View All Posts</inertia-link>
         </div>
-        <div v-for="(post, index) in posts" :key="post.slug" class="mb-4" :class="index < posts.length -1 ? 'border-b border-gray-200' : ''">
-            <div class="grid grid-cols-4 py-6">
-                <div>
-                    <div class="text-gray-500 text-sm">
-                        <div class="text-xl">
-                            {{ format_date(post.published_at, 'DD') }}
-                        </div>
-                        <div>
-                            {{ format_date(post.published_at, 'MMM YYYY') }}
-                        </div>
-                    </div>
-                </div>
-                <div class="col-span-2">
-                    <h3>
-                        <inertia-link :href="'/posts/posts/' + post.slug" class="text-2xl">{{ post.title}}</inertia-link>
-                    </h3>
-                    <div>
-                        <p class="text-sm">
-                            By <inertia-link class="text-gray-500 hover:text-yellow-500" :href="'/authors/authors/' + post.author.id">{{ post.author.name }}</inertia-link>
-                            in <inertia-link class="text-gray-500 hover:text-yellow-500" :href="'/categories/categories/' + post.category.slug">{{ post.category.name }}</inertia-link>
-                        </p>
-                        <p class="text-sm pt-2">
-                            {{ post.excerpt }}
-                        </p>
-
-                    </div>
-                </div>
-                <div class="flex flex-col justify-around">
-                    <div>
-                        <inertia-link :href="'/posts/posts/' + post.slug" class="py-2 px-4 border-2 border-black float-right hover:bg-black hover:text-gray-100">Continue Reading</inertia-link>
-                    </div>
-                </div>
-            </div>
+        <div class="text-center p-4">
+            <p class="text-xl mb-4">{{ author.name }}</p>
+            <img :src="author.image_url" :alt="author.name" width="350" class="mx-auto mb-8">
+            <p class="text-gray-500" v-html="author.bio" />
         </div>
+        <PostListing v-for="(post, index) in posts" :post="post" :key="index" class="mb-4" :class="index < posts.length -1 ? 'border-b border-gray-200' : ''" />
         <Pagination :links_data="links" />
     </div>
 </template>
@@ -47,16 +19,17 @@
 import moment from "moment";
 import Layout from "../../Shared/Layout";
 import Pagination from "../../Shared/Components/Pagination";
+import PostListing from "../../Shared/Components/PostListing";
 
 export default {
-    components: {Pagination},
+    components: {Pagination, PostListing},
     layout: Layout,
 
     name: "authors-index",
 
     title: 'Posts',
 
-    props: ['post_data', 'category'],
+    props: ['post_data', 'category', 'author', 'errors'],
 
     data: function() {
         return {
@@ -72,7 +45,3 @@ export default {
     }
 }
 </script>
-
-<style scoped>
-
-</style>
